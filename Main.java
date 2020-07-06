2ab7a6c0cffba11af41727aaadeecff1ebbf3f51
LenaGritse <65347294+LenaGritse@users.noreply.github.com>
2020-07-07 00:51:28 +0300

    commit2

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-package sample;
+package client;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Chat july 2020");
+        primaryStage.setScene(new Scene(root, 450, 275));
         primaryStage.show();
     }
 
